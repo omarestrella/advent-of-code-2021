144ab0fd41fdbe29c56a916bcaf59abd0f0ce574
Omar Estrella <oyestrella91@gmail.com>
2021-12-05 14:47:55 -0500

    Timing

--- Sources/advent/main.swift
+++ Sources/advent/main.swift
@@ -5,9 +5,12 @@ let group = DispatchGroup()
 DispatchQueue.global(qos: .default).async(group: group) {
   group.enter()
   Task {
+    let start = Date.now
     let (partOne, partTwo) = await Runner.shared.run(day: 4)
+    let end = Date.now
     print("Part 1: \(partOne)")
     print("Part 2: \(partTwo)")
+    print("Time: \(DateInterval(start: start, end: end).duration.formatted()) seconds")
     group.leave()
   }
 }
